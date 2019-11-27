be18ad3f5dedaa9def6d3b733fc14eaf053bdea4
Chris Newbill <chnewbil@microsoft.com>
2019-11-26 17:27:57 -0800

    Updates

---
+++ precache-manifest.7532f7c42cb51bbcc68d41ef50911430.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef69c18d85a15f337821137c2f2222c",
+    "url": "/TESCandyland/index.html"
+  },
+  {
+    "revision": "9d6f1e61112a364570df",
+    "url": "/TESCandyland/static/css/main.20fc688d.chunk.css"
+  },
+  {
+    "revision": "410ea2ed17502a93680d",
+    "url": "/TESCandyland/static/js/2.870d0343.chunk.js"
+  },
+  {
+    "revision": "9d6f1e61112a364570df",
+    "url": "/TESCandyland/static/js/main.5d166d34.chunk.js"
+  },
+  {
+    "revision": "3269b2355248f4978c34",
+    "url": "/TESCandyland/static/js/runtime-main.8f3c2358.js"
+  },
+  {
+    "revision": "e5b987bc5a3585df29c939e5a0023e7b",
+    "url": "/TESCandyland/static/media/board.e5b987bc.jpg"
+  },
+  {
+    "revision": "5bbf3e1aaafa0d2e6855fa3442539750",
+    "url": "/TESCandyland/static/media/player1.5bbf3e1a.png"
+  },
+  {
+    "revision": "5f5aa4546a71a0660491eb13f674c318",
+    "url": "/TESCandyland/static/media/player2.5f5aa454.png"
+  },
+  {
+    "revision": "d97658187aac4e2d2b65fc1623e8a9b3",
+    "url": "/TESCandyland/static/media/player3.d9765818.png"
+  },
+  {
+    "revision": "8d207f259f869115e215ad5f78f8d17a",
+    "url": "/TESCandyland/static/media/player4.8d207f25.png"
+  }
+]);
